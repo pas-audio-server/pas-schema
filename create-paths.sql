4fd0782d71cbd27e048bb6f4a564f3281853916c
pkivolowitz <perry@kivolowitz.com>
2017-04-23 14:40:43 -0500

    Delete create-paths.sql

--- create-paths.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `paths` (
-  `me` int(11) NOT NULL,
-  `up` int(11) NOT NULL,
-  `name` varchar(256) NOT NULL,
-  `namespace` varchar(16) NOT NULL,
-  PRIMARY KEY (`me`,`namespace`)
-) ENGINE=InnoDB DEFAULT CHARSET=ascii;
